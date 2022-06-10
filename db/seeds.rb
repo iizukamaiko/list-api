1fdc513e627948f6a5aa9fd086690481f702ac00
iizk <iizkmaiko@gmail.com>
2022-06-10 14:15:26 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+user = User.create(name: "Sample", email: "sample@gmail.com", password: "sample")
+
+list = List.create(title: "To Watch", icon_number: 1, user: user)
+
+todos = [
+  ["watch \" Sleepless in Seattle\"", true, user, list], 
+  ["watch \" You\'ve got mail\"", true, user, list], 
+  ["watch \" When Harry met Sally\"", false, user, list]
+]
+
+todos.each do |content, done, user, list| 
+  Todo.create(content: content, done: done, user: user, list: list)
+end
